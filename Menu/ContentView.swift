70c70b51c213397b6b8396ad72722451b8449ce5
Artem Paliutin <pal.artem@icloud.com>
2021-12-12 15:19:01 +0300

    Initial Commit

---
+++ Menu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Menu
+//
+//  Created by Artem Paliutin on 12.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Artem Paliutin on 12.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
